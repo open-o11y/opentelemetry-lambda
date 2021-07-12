814ffae59b177362b88d1073e8e66354e8d0aa1d
Karen Xu <karenyrxu@gmail.com>
2021-07-12 17:10:05 -0400

    add terraform config for sample app

---
+++ java/sample-apps/metrics-prometheus/deploy/main.tf
@@ -0,0 +1,46 @@
+module "hello-lambda-function" {
+  source = "terraform-aws-modules/lambda/aws"
+
+  function_name = var.name
+  handler       = "metrics.App::handleRequest"
+  runtime       = "java11"
+
+  create_package         = false
+  local_existing_package = "${path.module}/../../build/libs/aws-sdk-all.jar"
+
+  memory_size = 384
+  timeout     = 20
+
+  layers = compact([
+    var.collector_layer_arn,
+    var.sdk_layer_arn
+  ])
+
+  environment_variables = {
+    AWS_LAMBDA_EXEC_WRAPPER = "/opt/otel-proxy-handler"
+  }
+
+  tracing_mode = var.tracing_mode
+
+  attach_policy_statements = true
+  policy_statements = {
+    s3 = {
+      effect = "Allow"
+      actions = [
+        "s3:ListAllMyBuckets"
+      ]
+      resources = [
+        "*"
+      ]
+    }
+  }
+}
+
+module "api-gateway" {
+  source = "../../../../../utils/terraform/api-gateway-proxy"
+
+  name                = var.name
+  function_name       = module.hello-lambda-function.lambda_function_name
+  function_invoke_arn = module.hello-lambda-function.lambda_function_invoke_arn
+  enable_xray_tracing = var.tracing_mode == "Active"
+}
---
+++ java/sample-apps/metrics-prometheus/deploy/variables.tf
@@ -0,0 +1,23 @@
+variable "name" {
+  type        = string
+  description = "Name of created function and API Gateway"
+  default     = "hello-java-metrics-wrapper"
+}
+
+variable "collector_layer_arn" {
+  type        = string
+  description = "ARN for the Lambda layer containing the OpenTelemetry collector extension"
+  // TODO(anuraaga): Add default when a public layer is published.
+}
+
+variable "sdk_layer_arn" {
+  type        = string
+  description = "ARN for the Lambda layer containing the OpenTelemetry Java SDK"
+  // TODO(anuraaga): Add default when a public layer is published.
+}
+
+variable "tracing_mode" {
+  type        = string
+  description = "Lambda function tracing mode"
+  default     = "PassThrough"
+}
