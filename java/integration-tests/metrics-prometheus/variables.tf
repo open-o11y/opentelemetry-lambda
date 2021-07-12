987834710482333954a4a2b8f4d7878d4e456c11
Kelvin Lo <kello@live.ca>
2021-07-12 12:12:23 -0700

    add integ test terraform config

---
+++ java/integration-tests/metrics-prometheus/variables.tf
@@ -0,0 +1,17 @@
+variable "collector_layer_name" {
+  type        = string
+  description = "Name of published collector layer"
+  default     = "opentelemetry-collector"
+}
+
+variable "function_name" {
+  type        = string
+  description = "Name of sample app function / API gateway"
+  default     = "hello-dotnet-awssdk-wrapper"
+}
+
+variable "enable_collector_layer" {
+  type        = bool
+  description = "Enables building and usage of a layer for the collector. If false, it means either the SDK layer includes the collector or it is not used."
+  default     = true
+}
