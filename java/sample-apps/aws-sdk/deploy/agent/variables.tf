8c77ea353f28a035d4f0f7e3a3a6c7d5f1dfbc9b
Karen Xu <karenyrxu@gmail.com>
2021-07-30 17:56:04 -0400

    add collector and java-agent arn for testing

--- java/sample-apps/aws-sdk/deploy/agent/variables.tf
+++ java/sample-apps/aws-sdk/deploy/agent/variables.tf
@@ -7,12 +7,14 @@ variable "name" {
 variable "collector_layer_arn" {
   type        = string
   description = "ARN for the Lambda layer containing the OpenTelemetry collector extension"
+  default = "arn:aws:lambda:us-east-1:614732350472:layer:otel-collector-with-awsprw:3"
   // TODO(anuraaga): Add default when a public layer is published.
 }
 
 variable "sdk_layer_arn" {
   type        = string
   description = "ARN for the Lambda layer containing the OpenTelemetry Java Agent"
+  default = "arn:aws:lambda:us-east-1:901920570463:layer:aws-otel-java-agent-ver-1-2-0:2"
   // TODO(anuraaga): Add default when a public layer is published.
 }
 
